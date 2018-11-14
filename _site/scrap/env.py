5a884f1e4c2505f9815c51c7825189738281d4ed
seiyeon.kim <seiyeon.kim@osci.kr>
2018-11-15 02:18:05 +0900

    Add post

--- _site/scrap/env.py
+++ _site/scrap/env.py
@@ -4,4 +4,4 @@ env = dict()
 env['wiki_base_url'] = 'https://wiki.osci.kr'
 env['wiki_content_id'] = '44204609'
 env['wiki_id'] = 'seiyeon.kim'
-env['wiki_password'] = ''
+env['wiki_password'] = 'Qwerqwer1!'
